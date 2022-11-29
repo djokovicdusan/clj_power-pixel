2796cfbd5872736bd34bd42e5df6950fe40209a9
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-11-29 13:25:01 +0100

    added libraries and dependencies needed

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.drewnoakes/metadata-extractor "2.18.0"]
+                 [seesaw "1.5.0"]]
+  :main image-meta.ui
+  :uberjar-name "image-meta.jar"
+  :source-paths ["src"]
+  :profiles {:uberjar {:aot :all}}
   :repl-options {:init-ns clj-power-pixel.core})
