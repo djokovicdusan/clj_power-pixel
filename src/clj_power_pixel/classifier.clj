461755feaa9833e36f24522c51eb881314c11ab4
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-06 21:26:46 +0100

    dummy classifier done, returns a dummy photos path

--- src/clj_power_pixel/classifier.clj
+++ src/clj_power_pixel/classifier.clj
@@ -1,12 +1,12 @@
 (ns clj-power-pixel.classifier
   (:require [clj-power-pixel.metadata :as metadata]
-            [clj-power-pixel.files :as fs]
+            [clj-power-pixel.files :as nsfiles]
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(defn find-target-filenames
-  [target-dir files]
+(defn find-target-filenames-and-arrange-in-folder-by-class
+  [target-directory files]
   (let [filenames (map #(.getName %) files)
         captions (map metadata/get-caption-from-photo files)
         prefixes (map #(str/upper-case ((fnil first "_") %)) captions)] ;;edge case - no caption - will be "_" folder
-    (map #(str/join "/" [target-dir %1 %2]) prefixes filenames)))
+    (map #(str/join "/" [target-directory %1 %2]) prefixes filenames)))
