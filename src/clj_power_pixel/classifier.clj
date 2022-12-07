e42eb26286dd130ef25371c154fbb991f5b8d599
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-07 13:12:24 +0100

    arrange photos function added

--- src/clj_power_pixel/classifier.clj
+++ src/clj_power_pixel/classifier.clj
@@ -37,3 +37,8 @@
         target-filenames (find-target-filenames-and-arrange-in-folder-by-class target-directory files)]
     (zipmap files target-filenames)))
 
+(defn arrange-photos
+  [source-directory target-directory]
+  (doseq [[source-2 target-filename] (map-src-and-target-directories-with-caption-classifier source-directory target-directory)]
+    (io/make-parents target-filename)
+    (io/copy source-2 (io/file target-filename))))
