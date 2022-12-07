2696e0b32f0cead6c5ea40efa692352d6a0d7ee8
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-07 11:07:56 +0100

    added one more classifier - by artist

--- src/clj_power_pixel/classifier.clj
+++ src/clj_power_pixel/classifier.clj
@@ -4,9 +4,27 @@
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
+
+(defn find-target-filenames-and-arrange-in-folder-by-copyright
+  [target-directory files]
+  (let [filenames (map #(.getName %) files)
+        copyright (map metadata/get-copyright-from-photo files)]
+    (map #(str/join "/" [target-directory %1 %2]) copyright filenames)))
+
+(defn find-target-filenames-and-arrange-in-folder-by-artist
+  [target-directory files]
+  (let [filenames (map #(.getName %) files)
+        artist (map metadata/get-artist-from-photo files)]
+    (map #(str/join "/" [target-directory %1 %2]) artist filenames)))
+
+
 (defn find-target-filenames-and-arrange-in-folder-by-class
   [target-directory files]
   (let [filenames (map #(.getName %) files)
         captions (map metadata/get-caption-from-photo files)
         prefixes (map #(str/upper-case ((fnil first "_") %)) captions)] ;;edge case - no caption - will be "_" folder
     (map #(str/join "/" [target-directory %1 %2]) prefixes filenames)))
+
+
+
+
