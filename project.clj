a02735aa292e51646e39741182b7245dbb21b138
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-07 23:55:32 +0100

    added some simple unit tests for author getter

--- project.clj
+++ project.clj
@@ -2,12 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.drewnoakes/metadata-extractor "2.18.0"]
                  [seesaw "1.5.0"]]
-  :main image-meta.ui
-  :uberjar-name "image-meta.jar"
+  :main clj_power_pixel.ui
   :source-paths ["src"]
   :profiles {:uberjar {:aot :all}
              :dev
--- test/clj_power_pixel/core_test.clj
+++ test/clj_power_pixel/core_test.clj
@@ -1,6 +1,7 @@
 (ns clj-power-pixel.core-test
   (:require [clojure.test :refer :all]
             [clj-power-pixel.core :refer :all]
+            [clj-power-pixel.metadata :refer :all]
             [midje.sweet :refer :all]))
 
 (deftest a-test
@@ -8,4 +9,18 @@
     (is (= 0 1))))
 
 
-(facts "Najjednostavniji test" (+ 1 2) => 3)
+(facts "simple test"
+       (+ 1 2) => 3,
+       (+ 1 1) => 2)
+
+(facts "test author"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2111.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2162.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2284.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2298.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2320.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2329.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2333.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2338.jpg") => "DUSAN DJOKOVIC")
+
+
