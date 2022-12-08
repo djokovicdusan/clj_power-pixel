b5e6f0ae962fe6bbd22735b5f411f040fc843a9a
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-08 12:17:57 +0100

    load image function added

---
+++ src/clj_power_pixel/cv/cv.clj
@@ -0,0 +1,14 @@
+(ns clj-power-pixel.cv.cv
+  (:require [clojure.java.io :as io])
+  (:import [org.opencv.core Core Mat]
+           [org.opencv.highgui Highgui]
+           [org.opencv.imgproc Imgproc]
+           (clojure.lang RT)))
+
+(defonce opencv-native-loaded?
+         (RT/loadLibrary Core/NATIVE_LIBRARY_NAME))
+
+(defn load-image
+  [path]
+  (Highgui/imread path))
+
