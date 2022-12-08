b273700022e5bfc33221c78b4c732a61a9d8bed7
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-08 11:12:43 +0100

    added more caption tests

--- test/clj_power_pixel/core_test.clj
+++ test/clj_power_pixel/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [clj-power-pixel.core :refer :all]
             [clj-power-pixel.metadata :refer :all]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer :all])
+  (:import [com.drew.imaging ImageMetadataReader ImageProcessingException]))
 
 
 (facts "test author"
@@ -13,8 +14,19 @@
        (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2320.jpg") => "DUSAN DJOKOVIC"
        (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2329.jpg") => "DUSAN DJOKOVIC"
        (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2333.jpg") => "DUSAN DJOKOVIC"
-       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2338.jpg") => "DUSAN DJOKOVIC")
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/photos/MS__2338.jpg") => "DUSAN DJOKOVIC"
+       (clj-power-pixel.metadata/get-artist-from-photo "resources-test/dummy/json.js") => nil)
 
 (facts "test caption"
-       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2111.jpg") => "A")
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2111.jpg") => "A"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2162.jpg") => "A"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2284.jpg") => "B"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2298.jpg") => "C"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2320.jpg") => "A"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2329.jpg") => "B"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2333.jpg") => "C"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/photos/MS__2338.jpg") => "A"
+       (clj-power-pixel.metadata/get-caption-from-photo "resources-test/dummy/json.js") => nil)
+
+
 
