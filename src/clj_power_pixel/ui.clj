5e04b1583a1b0dace63df5b2ed16ad4a1b8b05b2
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-14 16:26:06 +0100

    started implementing simple ui using seesaw

--- src/clj_power_pixel/ui.clj
+++ src/clj_power_pixel/ui.clj
@@ -1,1 +1,41 @@
-(ns clj-power-pixel.ui)
+(ns clj-power-pixel.ui
+  (:require [clj-power-pixel.cv.cv :as nscv]
+            [seesaw.core :as ss]
+            [seesaw.mig :as mig]
+            [seesaw.chooser :as sc]
+            [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:gen-class))
+
+(def my-view
+  (mig/mig-panel
+    :constraints ["wrap 4, center, gap 15"]
+    :items [[(ss/label :icon (io/resource "icons/ic_folder.png")
+                       :id :src :text "Choose source directory") "span 4"]
+            [(ss/label :icon (io/resource "icons/ic_folder.png")
+                       :id :target :text "Choose target directory") "span 4"]
+            [(ss/checkbox :id :copy :selected? true) "span 1"]
+            [(ss/label :text "Copy files?") "span 3"]
+            [(ss/checkbox :id :cv :selected? true) "span 1"]
+            [(ss/label :text "Run plagiarism checker?") "span 3"]
+            [""]
+            [(ss/button :id :submit :text "Submit" :enabled? false)]]))
+
+(defn build-main-ui-frame
+  [exit?]
+  (ss/frame :title "Power Pixel tool by Dule Djo"
+            :size [500 :by 300]
+            :on-close (if exit? :exit :dispose)
+            :content my-view
+            :resizable? false))
+
+(defn ui-main
+  [exit?]
+  (ss/invoke-later                                          ;;call on ui thread
+    (-> (build-main-ui-frame exit?)
+        ss/show!)))
+
+(defn main
+  []
+  ;;replace with false if you don't want to kill REPL on exit
+  (ui-main true))
