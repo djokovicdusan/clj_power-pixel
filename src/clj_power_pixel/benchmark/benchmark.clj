b9f69fa7c3a96ce47067793689d59054d923d911
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-17 18:42:52 +0100

    minor updates

--- src/clj_power_pixel/benchmark/benchmark.clj
+++ src/clj_power_pixel/benchmark/benchmark.clj
@@ -1,1 +1,5 @@
-(ns clj-power-pixel.benchmark.benchmark)
+(ns clj-power-pixel.benchmark.benchmark
+  (:require [criterium.core :refer :all]
+            [clj-power-pixel.cv.cv :as nscv]
+            [clj-power-pixel.classifier :as nsclass]
+            [clj-power-pixel.files :as nsfiles]))
