01ef2f14fb0af46b4d7dca00e5b0483b55a6b401
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-17 18:46:11 +0100

    minor updates

--- src/clj_power_pixel/benchmark/benchmark.clj
+++ src/clj_power_pixel/benchmark/benchmark.clj
@@ -1,5 +1,10 @@
 (ns clj-power-pixel.benchmark.benchmark
-  (:require [criterium.core :refer :all]
+  (:require [criterium.core :as crit]
             [clj-power-pixel.cv.cv :as nscv]
             [clj-power-pixel.classifier :as nsclass]
             [clj-power-pixel.files :as nsfiles]))
+
+
+(crit/with-progress-reporting (crit/quick-bench (+ 1 1)))
+
+;(time ( 1000))
