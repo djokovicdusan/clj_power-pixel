c6a049a4047d14a7ea8a347d19db85e0c835227f
djokovicdusan <dusandjokovic.leboapps@gmail.com>
2022-12-24 14:56:01 +0100

    util files

---
+++ resources-test/dummy/json.js
@@ -0,0 +1,68 @@
+    
+    
+    
+    
+let obj = {
+"Zahtev za prijemno kontrolisanje, ispitivanje i dodelu statisa RM-a" : {
+	
+    "Broj" : "50-321/24",
+    "Sifra" : "KM ZA 05-1-01/05",
+    "NazivRM":"Repromaterijalarton rukohvat paket KM 6x1,751(PLU)",
+    "SifraRM-a" : 161182,
+    "SifraMagacina" : 50,
+    "NazivMagacina" : "CENTRALNI MAGACIN REPRO MATERIJALA",
+    "Datum i vreme prispeca" : "08.11.2021. 18:57",
+    "Proizvodjac" : {
+        "SifraProizvodjaca" : 9003043,
+        "NazivProizvodjaca" : "ZAPIS GRAFICKO-IZDAVACKO DOO"
+    },
+    "Dobavljac" : {
+        "NazivDobavljaca":"Prevoznik-Spediter Zapis",
+        "KamionBr":"ARO33HZ",
+        "OtpremnicaBr":"0868/21"
+    },
+    "Neto kolicina": 54000.00 ,
+    "Neto kolicina ustanovljena kod prijema": 54000.00,
+    "Nacin pakovanja":"Paket",
+    "Broj ambalaznih jedinica": 11.00,
+    "Kolicina jedinice pakovanja": 5000.00,
+    "Repromaterijal nabavio": "UVOZ/IZV NABAVKAX",
+    "Datum kreiranja": "08.11.2021",
+    "Zahtev izdao magacioner":"ZORAN KARADINOVIC",
+    "Datum azuriranja":"",
+    "Operater azuriranja":"",
+    "Zahtev primio QC":"potpis",
+    "Datum i vreme prijema zahteva": "08.11.2021 18:58"
+
+	}
+}
+
+
+let obj2 = 
+{
+    "Datum otvaranja naloga":"3. novembar 2021",
+    "Proizvodna linija":"PET 1",
+    "Pocetak sanitacije":{
+        "datum":"03.11.2021.",
+        "vreme":"8:00"
+    },
+    "Kraj sanitacije":{
+        "datum":"03.11.2021",
+        "vreme":"10:30"
+    },
+    "Tip":["VELIKO PRANJE","MALO PRANJE","DEZINFEKCIJA","STERILIZACIJA"],
+    "Opis":["pre AQUA VIVE","pre REMIX-a"],
+    "Specifikacija materijala/sredstava":[{
+        "Sifra":114586,
+        "Naziv repromaterijala":"P3-MIP C",
+        "JM":"kg",
+        "Kolicina":{
+            "utroseno":20
+        }
+    }],
+    "Vreme angazovanja min":"150.0 min",
+    "Vreme angazovanja h":"2.50 sati",
+    "Napomena":"napomena1",
+    "Odgovorno lice":"Radomir"
+
+}
